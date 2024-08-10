cfd55e0f57e867462670f97871e650b7575e748b
Ashik Rai <ashik.rai@lokibots.com>
2024-08-10 21:41:07 +0530

    First Commit 1. React Project 2. Profile Component

---
+++ src/Utils/Constants.js
@@ -0,0 +1,13 @@
+import defaultImage from "../resources/images/defaultImage.jpg"
+
+export const userProfileDefaultData={
+    "Name"      :"Ashik Rai",
+    "UserName"  :"ashik_rai",
+    "DOB"       :"27-04-2024",
+    "Address"   :"Electronic City, Bangalore",
+    "EmailID"   :"ashik@gmail.com",
+    "PhoneNo"   :"897465412",
+    "Description":"I'm a Software Engineer",
+    "Photo"     : defaultImage
+}
+
